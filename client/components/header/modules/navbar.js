3e945ea9028ccc58a19f76ebab7c872b81cb0669
Daniel <dcastillo3@gmail.com>
2018-11-01 23:12:26 -0400

    Style(landing): Animate transitions on landing component

--- client/components/header/modules/navbar.js
+++ client/components/header/modules/navbar.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
 import {logout} from '../../../store';
+import { Slide } from '@material-ui/core';
 import { smoothScroll } from '../../../utilities/utilities';
 
 class Navbar extends Component { 
@@ -18,26 +19,28 @@ class Navbar extends Component {
     const { handleClick, isLoggedIn } = this.props;
 
     return (
-      <div className="navbar flex-row flex-center palette-one-transparent flex-full-grid">
-        <div className="social-media menu-items flex-row flex-center">
-          <i class="fab fa-soundcloud"></i>
-          <i class="fab fa-instagram"></i>
-          <i class="fab fa-facebook-square"></i>
-          <i class="fab fa-youtube"></i>
-        </div>
+      <Slide direction="down" timeout={1000} in={true} style={{transitionDelay: 1000}}>
+        <div className="navbar flex-row flex-center palette-one-transparent flex-full-grid">
+          <div className="social-media menu-items flex-row flex-center">
+            <i class="fab fa-soundcloud"></i>
+            <i class="fab fa-instagram"></i>
+            <i class="fab fa-facebook-square"></i>
+            <i class="fab fa-youtube"></i>
+          </div>
 
-        <div className="logo"></div>
+          <div className="logo"></div>
 
-        <div className="nav-links menu-items">
-          <a onClick={(event) => this.onSectionClick(event)}>Home</a>
-          <a onClick={(event) => this.onSectionClick(event)}>Photos</a>
-          <a onClick={(event) => this.onSectionClick(event)}>Videos</a>
-          <a onClick={(event) => this.onSectionClick(event)}>Bio</a>
-          <a onClick={(event) => this.onSectionClick(event)}>Store</a>
-          {/* <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link> */}
+          <div className="nav-links menu-items">
+            <a onClick={(event) => this.onSectionClick(event)}>Home</a>
+            <a onClick={(event) => this.onSectionClick(event)}>Photos</a>
+            <a onClick={(event) => this.onSectionClick(event)}>Videos</a>
+            <a onClick={(event) => this.onSectionClick(event)}>Bio</a>
+            <a onClick={(event) => this.onSectionClick(event)}>Store</a>
+            {/* <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link> */}
+          </div>
         </div>
-      </div>
+      </Slide>
     )
   }
 }
--- client/components/main/sections/landing.js
+++ client/components/main/sections/landing.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {connect} from 'react-redux';
-import { Grow, Slide, Fade } from '@material-ui/core';
+import { Slide, Fade } from '@material-ui/core';
 
 /**
  * COMPONENT
@@ -12,12 +12,14 @@ export const Landing = props => {
   return (
     <div className="landing overlay-parent">
       <div className="landing-bg-layer"></div>
-      <Fade timeout={1000} in={true}>
+      <Fade timeout={3000} in={true}>
         <div className="landing-bismark-layer overlay-element"></div>
       </Fade>
+      <Slide direction="left" timeout={1000} in={true} style={{transitionDelay: 1500}}>
       <div className="landing-video-layer overlay-element flex-column flex-center">
         <div className="landing-video-layer-border"></div>
       </div>
+      </Slide> 
     </div>
   )
 }
