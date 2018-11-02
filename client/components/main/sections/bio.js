36a0a9bd8f350ad7d3c86cc6a9700426e6315902
Daniel <dcastillo3@gmail.com>
2018-11-02 05:39:28 -0400

    Style(style.css): Update palettes

--- client/components/main/sections/bio.js
+++ client/components/main/sections/bio.js
@@ -3,7 +3,7 @@ import React from 'react'
 export const Bio = props => {
 
   return (
-    <div className="bio flex-column flex-center palette-one">
+    <div className="bio flex-column flex-center palette-four">
       <h3>Bio</h3>
     </div>
   )
