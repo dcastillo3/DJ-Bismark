7a7cd7dba8bec32b3017348ab2ef714e01f01e09
Daniel Castillo <dcastillo3@gmail.com>
2018-11-03 18:57:37 -0400

    Feature(music.js): Embed music player into music componenet

--- client/components/main/sections/music.js
+++ client/components/main/sections/music.js
@@ -4,7 +4,14 @@ export const Music = props => {
 
   return (
     <div className="music flex-column flex-center palette-three">
-      <h3>Music</h3>
+      <iframe 
+        width="100%" 
+        height="300" 
+        scrolling="no" 
+        frameborder="no" 
+        allow="autoplay" 
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/users/94982824&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true">
+      </iframe>
     </div>
   )
 }
