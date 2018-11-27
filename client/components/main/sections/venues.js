9c890c619c8ca61010a49b3e4d2fb94cd89ef5b2
Daniel <dcastillo3@gmail.com>
2018-11-27 04:00:38 -0500

    Chore(venues.js): Migrate utilities function to utilities file and partition venues for slideshow rendering

--- client/components/main/sections/venues.js
+++ client/components/main/sections/venues.js
@@ -1,64 +1,66 @@
 import React, { Component } from 'react'
 import { Carousel } from 'react-responsive-carousel';
+import "style-loader!css-loader!react-responsive-carousel/lib/styles/carousel.min.css";
+import { partition, getFileName } from '../../../utilities';
 
 export class Venues extends Component {
   constructor(props) {
     super(props);
     this.state = {
       venues: [
-        {name: 'Moschino', image: 'images/venues/moschino.png'},
-        {name: 'Pianos', image: 'images/venues/pianos.png'},
-        {name: 'The Lately', image: 'images/venues/lately.png'},
-        {name: 'The Handy Liquor Bar', image: 'images/venues/handy.png'},
-        {name: 'Slate', image: 'images/venues/slate.png'},
-        {name: 'Space Ibiza: New York', image: 'images/venues/space.png'},
-        {name: 'Playstation Theater', image: 'images/venues/playstation.png'},
-        {name: 'Webster Hall: New York City', image: 'images/venues/webster.png'},
-        {name: 'Studio 151', image: 'images/venues/studio-151.png'},
-        {name: 'Thai Rock: New York City', image: 'images/venues/thai-rock.png'},
-        {name: 'La Esquina: New York City', image: 'images/venues/esquina.png'},
-      ],
-      count: 0
+        {name: 'Moschino', imageUrl: 'images/venues/moschino.png'},
+        {name: 'Pianos', imageUrl: 'images/venues/pianos.png'},
+        {name: 'The Lately', imageUrl: 'images/venues/lately.png'},
+        {name: 'The Handy Liquor Bar', imageUrl: 'images/venues/handy.png'},
+        {name: 'Slate', imageUrl: 'images/venues/slate.png'},
+        {name: 'Space Ibiza: New York', imageUrl: 'images/venues/space.png'},
+        {name: 'Playstation Theater', imageUrl: 'images/venues/playstation.png'},
+        {name: 'Webster Hall: New York City', imageUrl: 'images/venues/webster.png'},
+        {name: 'Studio 151', imageUrl: 'images/venues/studio-151.png'},
+        {name: 'Thai Rock: New York City', imageUrl: 'images/venues/thai-rock.png'},
+        {name: 'La Esquina: New York City', imageUrl: 'images/venues/esquina.png'},
+      ]
     }
   }
 
   render() {
     const { venues } =  this.state;
-    const { count } = this.state;
+    const partVenues = partition(venues, 4);
 
     return (
       <div id="venues" className="venues flex-column flex-center palette-three bottom-blue">
         <div className="venue-content flex-column flex-center narrow-content large-padding">
 
-        {/* <Carousel
+        <Carousel
           showStatus={false}
           showIndicators={false}
           showThumbs={false}
           infiniteLoop={true}
           autoplay={true}
           transitionTime={1000}
-        > */}
+        >
           
-          <div className="venue-items flex-row flex-center">
-            {venues.length && venues.map(venue => {
-              const { image } = venue;
-              
-              //Write optimal algorithm to find filename without extension
-              const lastDot = image.lastIndexOf('.') > -1 ? image.lastIndexOf('.') : image.length - 1;
-              const lastSlash = image.lastIndexOf('/') > -1 ? image.lastIndexOf('/') : 0;
-              const imageName = image.slice(lastSlash + 1, lastDot);
+          {partVenues.length && partVenues.map(venueArr => {
+            return (
+              <div className="venue-items flex-row flex-center">
 
-              const venueImage = {
-                backgroundImage: `url(${image})`
-              }
+                {venueArr.length && venueArr.map(venue => {
+                  const { imageUrl } = venue;
+                  const imageName = getFileName(imageUrl);
+                  const venueImage = {
+                    backgroundImage: `url(${imageUrl})`
+                  }
 
-              return (
-                  <div className={`venue-item-${imageName}`} style={venueImage}></div>
-              )
-            })}
-          </div>
+                  return (
+                    <div className={`venue-item-${imageName}`} style={venueImage}></div>
+                  )
+                })}
 
-        {/* </Carousel> */}
+              </div>
+            )
+          })}
+
+        </Carousel>
 
         </div>
       </div>
