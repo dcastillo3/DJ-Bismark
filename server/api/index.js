a7319bbc91738888144b184d75805a975e283d31
Daniel <dcastillo3@gmail.com>
2018-12-05 21:43:02 -0500

    Chore(index.js): Setup express routes to use images and videos directories

--- server/api/index.js
+++ server/api/index.js
@@ -3,6 +3,10 @@ module.exports = router
 
 router.use('/users', require('./users'))
 
+router.use('/images', require('./images'))
+
+router.use('/videos', require('./videos'))
+
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
