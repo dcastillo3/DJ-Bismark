6446357218fe1106a83492f8a600222e1ad1cedf
Daniel <dcastillo3@gmail.com>
2018-12-06 22:40:13 -0500

    Style(style.css): Create four columns in footer section

--- client/components/footer.js
+++ client/components/footer.js
@@ -1,12 +1,28 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import {logout} from '../store'
 
 const Footer = ({handleClick, isLoggedIn}) => (
-  <div className="footer flex-row flex-center">
-    <div className="logo"></div>
+  <div className="footer flex-row flex-center palette-two">
+
+    <div className="footer-columns flex-row large-padding narrow-content">
+      <div className="footer-column-a">
+        Hello
+      </div>
+
+      <div className="footer-column-b">
+        Hello 2
+      </div>
+
+      <div className="footer-column-c">
+        Hello 3
+      </div>
+      
+      <div className="footer-column-d">
+        Hello 4
+      </div>
+    </div>
+
+    {/* <div className="logo"></div>
     <nav className="nav-bar">
         <div className="nav-menu-items">
           <Link to="/home">Home</Link>
@@ -17,33 +33,8 @@ const Footer = ({handleClick, isLoggedIn}) => (
           <Link to="/login">Login</Link>
           <Link to="/signup">Sign Up</Link>
         </div>
-    </nav>
+    </nav> */}
   </div>
 )
 
-/**
- * CONTAINER
- */
-const mapState = state => {
-  return {
-    isLoggedIn: !!state.user.id
-  }
-}
-
-const mapDispatch = dispatch => {
-  return {
-    handleClick() {
-      dispatch(logout())
-    }
-  }
-}
-
-export default connect(mapState, mapDispatch)(Footer)
-
-/**
- * PROP TYPES
- */
-Footer.propTypes = {
-  handleClick: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired
-}
+export default Footer
