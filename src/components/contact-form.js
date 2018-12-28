ee6e8df5db929100b68a5a5b22ff0a3a144d87f8
Daniel <dcastillo3@gmail.com>
2018-12-27 20:20:26 -0500

    Migrate(Contact-Form): Migrate contact form layout

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -1,57 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export const ContactForm = props => {
+export class ContactForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      First: { value: '', type: 'text', required: 'true'},
+      Last: { value: '', type: 'text', required: 'true' },
+      Email: { value: '', type: 'email', required: 'true' },
+      Phone: { value: '', type: 'number', required: 'true' },
+      Organization: { value: '', type: 'text', required: 'true' },
+      Venue: { value: '', type: 'text', required: 'true' },
+      Location: { value: '', type: 'text', required: 'true' },
+      Date: { value: '', type: 'date', required: 'true' },
+      Comments: { value: '', type: 'text', required: 'false' }
+    }
+  }
 
-  return (
-    <div id="contact" className="contact-form-container flex-column flex-center palette-three">
-        <form className="flex-row flex-center large-padding narrow-content">
-          <div className="form-group flex-column">
-            <label>First</label>
-            <input />
+  contactFormChange = (e) => {
+    const { name } = e.target;
+    const { value } = e.target;
+    const formChange = { 
+      [name]: {...this.state[name]}
+    };
+
+    formChange[name].value = value;
+    this.setState(formChange);
+  }
+
+  contactFormSubmit = (e) => {
+    e.preventDefault();
+  }
+
+  render() {
+    const contactForm = this.state;
+    const fields = Object.keys(this.state);
+    console.log(this.state)
+
+    return (
+      <div id="contact" className="contact-form-section flex-column flex-center palette-three">
+          
+          <div className="contact-form-container flex-row flex-center large-padding narrow-content">
+            <div className="contact-form-text-container flex-column flex-center">
+              <div className="contact-form-text title">Contact DJ Bismark.</div>
+              <div className="contact-form-text-icon fas fa-envelope"></div>
+            </div>
+
+            <form onSubmit={this.contactFormSubmit} className="contact-form flex-row flex-center">
+
+              {fields.map(field => {
+                if (field !== 'Comments') {
+                  return (
+                    <div key={field} className="contact-form-input form-group flex-column">
+                      <input type={contactForm[field].type} value={contactForm[field].value} name={field} placeholder={field} onChange={this.contactFormChange} required={contactForm[field].required} />
+                    </div>
+                  )
+                } else {
+                  return (
+                    <div key={field} className="contact-form-textarea form-group flex-column">
+                      <textarea type={contactForm[field].type} value={this.state.Comments.value} name="Comments" placeholder="Comments" onChange={this.contactFormChange} required={contactForm[field].required} />
+                    </div>
+                  )
+                }
+              })
+              }
+
+              <button className="button" type="submit" value="submit">Submit</button>
+            </form>
           </div>
 
-          <div className="form-group flex-column">
-            <label>Last</label>
-            <input />
-          </div>
+      </div>
+    )
 
-          <div className="form-group flex-column">
-            <label>Email</label>
-            <input />
-          </div>
-
-          <div className="form-group flex-column">
-            <label>Phone</label>
-            <input />
-          </div>
-
-          <div className="form-group flex-column">
-            <label>Organization</label>
-            <input />
-          </div>
-
-          <div className="form-group flex-column">
-            <label>Venue</label>
-            <input />
-          </div>
-
-          <div className="form-group flex-column">
-            <label>Location</label>
-            <input />
-          </div>
-
-          <div className="form-group flex-column">
-            <label>Date</label>
-            <input />
-          </div>
-
-          <div className="form-group flex-column">
-            <label>Comments</label>
-            <textarea />
-          </div>
-        </form>
-    </div>
-  )
+  }
 }
 
-export default ContactForm;
+export default ContactForm
