bb04071efeaaee44361e7b6f61642b553f085f71
Daniel Castillo <dcastillo3@gmail.com>
2019-06-21 14:12:20 -0400

    Chore(bookings.js): Succesfully created get and update routes for booking requests

--- server/api/bookings.js
+++ server/api/bookings.js
@@ -4,23 +4,55 @@ const { utility } = require('../utilities');
 
 // MongoDB: Save booking request
 router.post('/request', (req, res, next) => {
-    let bookingInfo;
-
-    if(utility.validBookingRequest(req.body)) {
-        bookingInfo = utility.parseUnconfirmedBookingRequest(req.body);
-    } else {
-        throw new Error('Invalid booking format');
-    }
-
+    let bookingInfo = utility.parseBookingRequest(req.body);
     const currBookingRequest = new BookingRequest(bookingInfo);
 
     currBookingRequest.save((err, data) => {
-        if(err) console.log(err);
+        if(err) {
+            console.log(err);
+            res.status(500).send(err.message);
+        }
         else {
             console.log('Successfully saved booking request: ', data._id);
             res.sendStatus(201);
         }
     })
 });
+
+// MongoDB: Get booking requests
+router.get('/request', (req, res, next) => {
+    let query = req.query;
+
+    //get all booking requests that match query
+    BookingRequest.find(query, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send(err.message);
+        }
+        else {
+            console.log('Successfully queried!');
+            res.status(201).send(data);
+        }
+    })
+})
+
+// MongoDB: Update booking request
+router.put('/request', (req, res, next) => {
+    let bookingInfo = utility.parseBookingRequest(req.body);
+    let { _id } = bookingInfo;
+    let idQuery = { _id };
+
+    //get booking request with id and update
+    BookingRequest.findOneAndUpdate(idQuery, bookingInfo, (err, data) => {
+        if(err) {
+            console.log(err);
+            res.status(500).send(err.message);
+        }
+        else {
+            console.log('Successfully updated booking request: ', data._id);
+            res.sendStatus(201);
+        }
+    })
+})
 
 module.exports = router;
