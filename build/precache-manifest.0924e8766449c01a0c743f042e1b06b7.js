4e350b3ec3dbeab3b83db19560a51576d5de5333
Daniel <dcastillo3@gmail.com>
2019-07-16 17:57:45 -0400

    Chore(style): Update button and clean up centering

---
+++ build/precache-manifest.0924e8766449c01a0c743f042e1b06b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1c906a18b35636010f1e",
+    "url": "/static/js/main.1c906a18.chunk.js"
+  },
+  {
+    "revision": "2aca2d9138fa36f78cd6",
+    "url": "/static/js/1.2aca2d91.chunk.js"
+  },
+  {
+    "revision": "2aca2d9138fa36f78cd6",
+    "url": "/static/css/1.b93f567b.chunk.css"
+  },
+  {
+    "revision": "75bf7edaac74f56dd96b88962b662435",
+    "url": "/index.html"
+  }
+];
